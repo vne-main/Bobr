24acd801083bb9c41bec0b5311fc8706bae1266c
vasen <vasenking@mail.ru>
2019-02-08 22:25:55 +0500

    Делал не в той ветке, но сделал комментарии

---
+++ client/src/Components/Post/Comments/index.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import './style.css';
+import spImg2 from '../../../Static/img/sponsor/sponsor_2.jpg'
+import arrowImg from "../../../Static/img/stats/arrow.png";
+
+
+class Comments extends Component {
+    render(){
+        return (
+            <div className="comment_box">
+                <h3 className="title_h3 h3_com">Комментарии <i>90</i></h3>
+                <aside className="comment">
+                    <div className="comment_title">
+                        <div className="top_user">
+                            <img src={spImg2} alt="user_icon" className="user_icon"/>
+                            <p className="user_name">Оставил комментарий</p>
+                            <p className="news_time">примерно вчера</p>
+                        </div>
+                        <div className="news_vote">
+                            <img src={arrowImg} alt="arrowUp"/>
+                            <i>+2</i>
+                            <img src={arrowImg} alt="arrowDown" className="stats_arrow_down"/>
+                        </div>
+                    </div>
+                    <p>Ничего не понял, но очень интересно</p>
+                </aside>
+                <h3 className="title_h3">Написать комментарий</h3>
+                <div className="send_comment">
+                    <textarea />
+                    <button className="blue_button">Отправить</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Comments;
--- client/src/Components/StaticComponents/RightColumn/index.js
+++ client/src/Components/StaticComponents/RightColumn/index.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
 import './style.css';
 
+import spImg1 from '../../../Static/img/sponsor/sponsor_1.jpg'
+import spImg2 from '../../../Static/img/sponsor/sponsor_2.jpg'
+import spImg3 from '../../../Static/img/sponsor/sponsor_3.png'
+
 export default class RightColumn extends Component {
+
+    state = {
+        sponsors: [
+            {title: "Мой компьютер", text: "Компьютер, на котором я пишу этот код", img: spImg1},
+            {title: "Хорошее настроение", text: "Хорошее настроение - ключ к успеху", img: spImg2},
+            {title: "Поставленные цели", text: "Достинув цели, не нужно останавливаться", img: spImg3},
+        ]
+    };
+
     render() {
         return (
             <section className="main_right">
-                <div className="advertising">
+
+                <div className="right_column_box advertising">
                     Банер для рекламы
                 </div>
-                <div className="right_streams">
+
+
+                <div className="right_column_box">
+                    <h3>Спонсоры Бобра</h3>
+
+                    {this.state.sponsors.map((el, i) => {
+                        return (
+                            <div className="sponsor_box" key={i}>
+                                <div className="title_sponsor">
+                                    <h5>{el.title}</h5>
+                                    <img src={el.img} alt="sponsorImg"/>
+                                </div>
+                                <p>{el.text}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="right_column_box">
                     <h3>Потоки</h3>
                     <ol className="right_streams_list">
                         <li>Разработка</li>
