b7c0d1fe7d454659155860df4a53a57d8ba32aae
vasen <vasenking@mail.ru>
2019-02-12 15:49:01 +0500

    listen subdomain

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const http = require('http');
+
 const express = require('express');
 const logger = require('morgan');
 const BodyParser = require('body-parser');
@@ -17,6 +19,16 @@ app.use(BodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
 apiRouter(app);
 
-app.listen(3012, function () {
-    console.log("listen port: 3012")
-});
+http.createServer((req, res) => {
+    switch(req.headers.host) {
+        case 'vasenkin.ru':
+            app(req, res);
+            break;
+    }
+}).listen(3012);
+
+
+
+// app.listen(3012, function () {
+//     console.log("listen port: 3012")
+// });
