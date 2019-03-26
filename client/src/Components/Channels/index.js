fe2a52c660e421990bc6e5fb1f79108670b8b7fd
vasen <vasenking@mail.ru>
2019-03-26 16:08:06 +0500

    WebSocket channel

--- client/src/Components/Channels/index.js
+++ client/src/Components/Channels/index.js
@@ -10,15 +10,63 @@ import {connect} from "react-redux";
 
 class Channels extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            ws: new WebSocket('ws://localhost:3001'),
+            message: "",
+            list: ['Привет'],
+        }
+    }
+
+    webSocket() {
+        const {ws} = this.state;
+
+        const write = (message) => {
+            let newList = [...this.state.list];
+            newList.push(message);
+            this.setState({list: newList});
+        };
+
+        ws.onopen = function () {
+            console.info("Connect");
+        };
+
+        ws.onmessage = function (e) {
+            const message = e.data;
+            write(message);
+        };
+
+        ws.onerror = function (err) {
+            console.error(err);
+        };
+    }
+
+    add(){
+        const {ws, message} = this.state;
+        ws.send(JSON.stringify(message));
+        console.info("New message");
+    }
+
     componentDidMount() {
+        this.webSocket();
         this.props.changeCurrentPage("channels");
     }
 
     render() {
+        const {message, list} = this.state;
         return (
             <section>
-
-
+                <ol>
+                    {list.map((el,i) =>{
+                        return (<li key={i}>{el}</li>)
+                    })}
+                </ol>
+                <textarea
+                    value={message}
+                    onChange={(e) => this.setState({message: e.target.value})}
+                />
+                <button onClick={() => this.add()}>Send</button>
             </section>
         )
     }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const logger = require('morgan');
-const BodyParser = require('body-parser');
-const postRouter = require('./routes/post-routes');
-const userRouter = require('./routes/user-routes');
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const logger = require('morgan'),
+      BodyParser = require('body-parser'),
+      postRouter = require('./routes/post-routes'),
+      userRouter = require('./routes/user-routes'),
+      express = require('express'),
+      cors = require('cors'),
+      app = express();
 
 app.use(cors({
     origin: "http://localhost",
@@ -15,6 +15,27 @@ app.use(BodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
 postRouter(app);
 userRouter(app);
+
 app.listen(3000, function () {
     console.log("listen port: 3000")
+});
+
+const WebSocket = require('ws'),
+    wss = new WebSocket.Server({ port: 3001});
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        const data = JSON.parse(message);
+        if (data === "close"){
+            wss.close();
+            console.info('close');
+        } else {
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN){
+                    client.send(data);
+                }
+            });
+        }
+    });
+    ws.send('Something');
 });
