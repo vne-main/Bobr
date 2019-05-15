c5f846b2e87cf86a5a9c02ebe794e5dd1dfcf162
vasen <vasenking@mail.ru>
2019-05-15 06:47:50 +0500

    Build project

---
+++ client/build/precache-manifest.437078e6477e1170acb0da35b612056d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d49f58087094124c15f5340e632ba56b",
+    "url": "/static/media/work_3.d49f5808.svg"
+  },
+  {
+    "revision": "9b44d850f8fd3f79c9dd",
+    "url": "/static/css/main.faa9986f.chunk.css"
+  },
+  {
+    "revision": "9d8efe97a2db7fd5705a15c4dab0b0d5",
+    "url": "/static/media/screen_5.9d8efe97.jpg"
+  },
+  {
+    "revision": "fc8130c18483d2fde0c9",
+    "url": "/static/js/1.fc8130c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afb0867b98476552e67f325533b5b7d2",
+    "url": "/static/media/404.afb0867b.png"
+  },
+  {
+    "revision": "2a74d170777e51c176b3a8720b874838",
+    "url": "/static/media/user.2a74d170.svg"
+  },
+  {
+    "revision": "f68ee832b7b6847adb5d49ab40d7dcbe",
+    "url": "/static/media/work_1.f68ee832.svg"
+  },
+  {
+    "revision": "3866b43e96ece4f58be34b5dff646453",
+    "url": "/static/media/work_2.3866b43e.svg"
+  },
+  {
+    "revision": "9b44d850f8fd3f79c9dd",
+    "url": "/static/js/main.9b44d850.chunk.js"
+  },
+  {
+    "revision": "55bc3c0de0415f6d794dfb70683b6dc4",
+    "url": "/static/media/work_4.55bc3c0d.svg"
+  },
+  {
+    "revision": "324ffc3e1e757c1bbfdd03cd4302b20c",
+    "url": "/static/media/work_5.324ffc3e.svg"
+  },
+  {
+    "revision": "36e14a9bd18d23a16107815114db0939",
+    "url": "/static/media/screen_1.36e14a9b.jpg"
+  },
+  {
+    "revision": "908e4dba59336f5354b924db0d74eccc",
+    "url": "/static/media/screen_2.908e4dba.jpg"
+  },
+  {
+    "revision": "823e416aa9aaf1dcf9963cce9e6c5919",
+    "url": "/static/media/screen_3.823e416a.jpg"
+  },
+  {
+    "revision": "cad407f4ab674fca32fb6db4ef80ad22",
+    "url": "/static/media/screen_4.cad407f4.jpg"
+  },
+  {
+    "revision": "fc8130c18483d2fde0c9",
+    "url": "/static/css/1.92f47273.chunk.css"
+  },
+  {
+    "revision": "1921cdbb703edbb1066269450c4ad47b",
+    "url": "/index.html"
+  }
+];
