7ef5d67b1655b54550d3e1e0f74c92576ff92c8c
vasen <vasenking@mail.ru>
2019-06-16 20:19:12 +0500

    change port client / Mobile Panel&Header

--- client/src/Components/StaticComponents/Header/MobileHeader/index.js
+++ client/src/Components/StaticComponents/Header/MobileHeader/index.js
@@ -7,36 +7,26 @@ import BurgerIcon from '../../../../Static/img/header/burger.svg';
 /* Module */
 import Drawer from "@material-ui/core/Drawer/Drawer";
 import {Link} from "react-router-dom";
-import SearchIcon from '@material-ui/icons/Search';
 
-class MenuContent extends Component {
-
-    render() {
-        const {navigation, closeCallback} = this.props;
-        return (
-            <div className="menu-item">
-                <h3 className="header_logo mobile_burger_title">bobr</h3>
-                <Link to="/search" onClick={closeCallback}>
-                    <div className="burger_search">
-                        <span>Поиск</span>
-                        <SearchIcon/>
-                    </div>
-                </Link>
-                {navigation.map((el, i) => {
-                    return (
-                        <Link
-                            key={i}
-                            to={el.link}
-                            onClick={closeCallback}
-                        >
-                            {el.title}
-                        </Link>
-                    )
-                })}
-            </div>
-        )
-    }
-}
+const HeaderContent = ({navigation, closeCallback}) => {
+    return (
+        <div className="menu-item">
+            <h3 className="header_logo mobile_burger_title">bobr</h3>
+            <Link to="/" onClick={closeCallback}>Главная</Link>
+            {navigation.map((el, i) => {
+                return (
+                    <Link
+                        key={i}
+                        to={el.link}
+                        onClick={closeCallback}
+                    >
+                        {el.title}
+                    </Link>
+                )
+            })}
+        </div>
+    )
+};
 
 const MobileHeader = ({navigation}) => {
     const [menuOpen, changeOpen] = useState(false);
@@ -54,7 +44,7 @@ const MobileHeader = ({navigation}) => {
                 className="mobile_header"
                 anchor="left"
             >
-                <MenuContent
+                <HeaderContent
                     closeCallback={() => changeOpen(false)}
                     navigation={navigation}
                 />
--- client/src/Components/StaticComponents/Header/MobilePanel/index.js
+++ client/src/Components/StaticComponents/Header/MobilePanel/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {Component, useState} from 'react'
 import './style.css';
 
 /* Icon */
@@ -7,8 +7,26 @@ import BurgerIcon from '../../../../Static/img/header/panel.svg';
 /* Module */
 import Drawer from "@material-ui/core/Drawer/Drawer";
 import {Link} from "react-router-dom";
+import SearchIcon from '@material-ui/icons/Search';
 
-const MobileHeader = () => {
+const PanelContent = ({closeCallback}) => {
+    return (
+        <div className="menu-item">
+            <h3 className="header_logo mobile_burger_title">Панель</h3>
+            <Link to="/search" onClick={closeCallback}>
+                <div className="burger_search">
+                    <span>Поиск</span>
+                    <SearchIcon/>
+                </div>
+            </Link>
+            <Link to="/signin" onClick={closeCallback}>
+                Выход
+            </Link>
+        </div>
+    )
+};
+
+const MobilePanel = () => {
     const [menuOpen, changeOpen] = useState(false);
     return (
         <>
@@ -24,15 +42,10 @@ const MobileHeader = () => {
                 className="mobile_header"
                 anchor="right"
             >
-                <div className="menu-item">
-                    <h3 className="header_logo mobile_burger_title">Панель</h3>
-                    <Link to="/signin" onClick={() => changeOpen(false)}>
-                        Выход
-                    </Link>
-                </div>
+                <PanelContent closeCallback={() => changeOpen(false)}/>
             </Drawer>
         </>
     )
 };
 
-export default MobileHeader;
+export default MobilePanel;
