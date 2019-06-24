03f83ea70a198434b1d7a0f1760d2b44af269ebe
vasen <vasenking@mail.ru>
2019-06-24 09:14:10 +0500

    changes App

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from "react";
 /* Module */
 import {Route, BrowserRouter, Switch} from 'react-router-dom';
 
+
 /* Components */
 import CatchError from './Components/StaticComponents/CatchError';
 import RightColumn from './Components/RightColumn';
