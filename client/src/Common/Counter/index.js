95cfa97ca1dd2d45463f9e241e672d643a933c99
n-vasenkin <vasenking@Mail.ru>
2020-06-29 20:54:57 +0500

    start bobr refactoring

--- client/src/Common/Counter/index.js
+++ client/src/Common/Counter/index.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Counter extends Component {
-  render() {
-    const { value } = this.props;
-    let colorVote = {
-      color: value >= 0 ? (value === 0 ? '#548eaa' : '#6c9007') : '#d53c30'
-    };
-    return (
-      <i style={colorVote}>
-        {value > 0 && <b>+</b>}
-        {value}
-      </i>
-    );
-  }
-}
+export default ({ value }) => {
+  let colorVote = {
+    color: value >= 0 ? (value === 0 ? '#548eaa' : '#6c9007') : '#d53c30'
+  };
+  return (
+    <i style={colorVote}>
+      {value > 0 && <b>+</b>}
+      {value}
+    </i>
+  );
+};
--- client/src/Common/Footer/index.js
+++ client/src/Common/Footer/index.js
@@ -1,57 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <section className="footer_information">
-          <div className="container">
-            <div className="footer_half">
-              <div className="footer_column">
-                <h3>Аккаунт</h3>
-                <Link to="signin">Войти</Link>
-                <Link to="signup">Регистрация</Link>
-              </div>
-              <div className="footer_column">
-                <h3>Разделы</h3>
-                <Link to="/">Главная</Link>
-                <Link to="/publish">Опубликовать</Link>
-                <Link to="/users">Пользователи</Link>
-                <Link to="/chat">Чат</Link>
-              </div>
+export default () => {
+  return (
+    <footer>
+      <section className="footer_information">
+        <div className="container">
+          <div className="footer_half">
+            <div className="footer_column">
+              <h3>Аккаунт</h3>
+              <Link to="signin">Войти</Link>
+              <Link to="signup">Регистрация</Link>
             </div>
-            <div className="footer_half">
-              <div className="footer_column">
-                <h3>Информация</h3>
-                <ol>
-                  <li>Правила</li>
-                  <li>Помощь</li>
-                  <li>Документация</li>
-                </ol>
-              </div>
-              <div className="footer_column">
-                <h3>Реклама</h3>
-                <ol>
-                  <li>Реклама</li>
-                  <li>Контент</li>
-                  <li>Тарифы</li>
-                </ol>
-              </div>
+            <div className="footer_column">
+              <h3>Разделы</h3>
+              <Link to="/">Главная</Link>
+              <Link to="/publish">Опубликовать</Link>
+              <Link to="/users">Пользователи</Link>
+              <Link to="/chat">Чат</Link>
             </div>
           </div>
-        </section>
-        <section className="footer_bottom">
-          <div className="container">
-            <p>© 2019 by Nikolay</p>
-            <span>Настройки языка</span>
-            <span>О сайте</span>
-            <span>Служба поддержки</span>
-            <span>Мобильная версия</span>
+          <div className="footer_half">
+            <div className="footer_column">
+              <h3>Информация</h3>
+              <ol>
+                <li>Правила</li>
+                <li>Помощь</li>
+                <li>Документация</li>
+              </ol>
+            </div>
+            <div className="footer_column">
+              <h3>Реклама</h3>
+              <ol>
+                <li>Реклама</li>
+                <li>Контент</li>
+                <li>Тарифы</li>
+              </ol>
+            </div>
           </div>
-        </section>
-      </footer>
-    );
-  }
-}
+        </div>
+      </section>
+      <section className="footer_bottom">
+        <div className="container">
+          <p>© 2019 by Nikolay</p>
+          <span>Настройки языка</span>
+          <span>О сайте</span>
+          <span>Служба поддержки</span>
+          <span>Мобильная версия</span>
+        </div>
+      </section>
+    </footer>
+  );
+};
